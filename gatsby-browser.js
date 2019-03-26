38e648f3a9c821bd1de7b00b85a23a4c7dba7eab
Luke Bennett <luke.bennett88@gmail.com>
2019-03-27 10:53:29 +1100

    Init

---
+++ gatsby-browser.js
@@ -0,0 +1,13 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+// Self host fonts using Typefaces.js https://github.com/KyleAMathews/typefaces
+
+// Load Monoton typeface
+require('typeface-monoton');
+
+// Load Ramaraja typeface
+require('typeface-ramaraja');
